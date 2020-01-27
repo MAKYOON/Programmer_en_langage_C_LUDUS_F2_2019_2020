5686f22530a4fec41331d21b50c217e3fc8aab32
MAKYOON <k.li@ludus-academie.com>
2020-01-27 14:20:42 +0100

    Initialisation SDL2 en C

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <SDL2/SDL.h>
+
+
+int main(int argc, char* argv[])
+{
+    SDL_Window *pWindow=NULL;
+    SDL_Renderer *pRenderer=NULL;
+    SDL_Texture *pTexture=NULL;
+
+    return 0;
+}
