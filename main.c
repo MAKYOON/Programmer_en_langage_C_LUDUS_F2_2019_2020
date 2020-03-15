5ce50c5ffdfe3c394f46465264b284dc538dd326
MAKYOON <k.li@ludus-academie.com>
2020-03-15 10:04:09 +0100

    Partie 2 Question 1

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "sdl.h"
 #include "circle.h"
+#include <SDL2/SDL_Image.h>
 
 int main(int argc, char* argv[])
 {
@@ -8,30 +9,7 @@ int main(int argc, char* argv[])
     SDLManager.pWindow=NULL; //Pointeur typedef SDL_Window possède la référence d'un window(Win32)
     SDLManager.pRenderer=NULL; //Pointeur typedef SDL_RENDERER possède la référence du rendu encapsulé dans Window
 
-    SDL_Rect rectForSurface = {200,200,150,150};
-    SDL_Rect emptySquare = {400,200,150,150};
-    SDL_Rect filledSquare = {600,200,150,150};
-
-    //radius of the circle
-    int r = 100;
-    //x coordinate of the point to place
-    int x = 0;
-    //y coordinate of the point to place
-    int y = r;
-    //decision parameter to decide where we should place the pixel according to Bresenham's algorithm
-    int d = 3 - (2*r);
-    //x coordinate of the center of the circle 1
-    int xc1 = 250;
-    //y coordinate of the center of the circle 1
-    int yc1 = 500;
-
-    //x coordinate of the center of the circle 2
-    int xc2 = 500;
-    //y coordinate of the center of the circle 2
-    int yc2 = 500;
-
-
-
+	int quit = 0;
 
     //Initialisation d'SDL
     if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
@@ -44,39 +22,29 @@ int main(int argc, char* argv[])
     		//create renderer
     		SDLManager.pRenderer = SDL_CreateRenderer(SDLManager.pWindow,-1,SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
     	}
+		//loading a PNG with IMG_Load function, transforming it into a texture and rendering it
+	    SDL_Surface *myPNG = IMG_Load("character.png");
+	    SDL_Texture *PNGintoTexture = SDL_CreateTextureFromSurface(SDLManager.pRenderer,myPNG);
+		while (!quit)
+	    {	
+	    	//number of miliseconds since we started the program
+	    	int ticks = SDL_GetTicks();
+	    	//this is the time it takes to render one image from the sprite
+	    	int sprite = (ticks/200) % 8;
+	    	//the rect we cut from the sprite
+    		SDL_Rect srcrect = { sprite*33 , 0, 34, 40 };
+    		//the rect in which we paste the sprite
+			SDL_Rect dstrect = { 200, 200, 34, 40};
+	     	//red background
+	    	SDL_SetRenderDrawColor(SDLManager.pRenderer,255,0,0,SDL_ALPHA_OPAQUE);
+	    	SDL_RenderClear(SDLManager.pRenderer);
+		    SDL_RenderCopy(SDLManager.pRenderer,PNGintoTexture,&srcrect,&dstrect);
 
-    	//red background
-    	SDL_SetRenderDrawColor(SDLManager.pRenderer,255,0,0,SDL_ALPHA_OPAQUE);
-    	SDL_RenderClear(SDLManager.pRenderer);
-    	//setting back the color to black in order to draw
-    	SDL_SetRenderDrawColor(SDLManager.pRenderer,0,0,0,SDL_ALPHA_OPAQUE);
-    	//drawing empty square
-    	SDL_RenderDrawRect(SDLManager.pRenderer,&emptySquare);
-    	//drawing filled square
-    	SDL_RenderFillRect(SDLManager.pRenderer,&filledSquare);
-    	//Drawing empty circle according to Bresenham's algorithm
-    	DrawEmptyCircle(x,y,xc1,yc1,d,SDLManager.pRenderer);
-
-    	//drawing filled circle
-    	DrawFilledCircle(x,y,r,xc2,yc2,SDLManager.pRenderer);
-
-    	//Drawing a line
-    	SDL_RenderDrawLine(SDLManager.pRenderer,700,500,800,600);
-
-
-    	//creating the surface, changing it to a texture and displaying it
-    	SDL_Surface *myBMP = SDL_LoadBMP("chat.bmp");
-    	SDL_Texture *BMPintoTexture = SDL_CreateTextureFromSurface(SDLManager.pRenderer,myBMP);
-	    SDL_RenderCopy(SDLManager.pRenderer,BMPintoTexture,NULL,&rectForSurface);
-
-	    //updating the render to display everything we drew
-	    SDL_RenderPresent(SDLManager.pRenderer);
-	    SDL_Delay(5000);
-
-	    //we destroy in the opposite order that we created things
-	    SDL_DestroyTexture(BMPintoTexture);
-	    SDL_FreeSurface(myBMP);
-
+		    //updating the render to display everything we drew
+		    SDL_RenderPresent(SDLManager.pRenderer);
+		
+	    }
+	    
     	SDL_DestroyRenderer(SDLManager.pRenderer);
     	SDL_DestroyWindow(SDLManager.pWindow);
     	SDL_Quit();
@@ -89,5 +57,7 @@ int main(int argc, char* argv[])
 
 
     return 0;
+
+
 
 }
